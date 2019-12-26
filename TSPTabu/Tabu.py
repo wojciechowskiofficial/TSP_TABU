cc23bc00eb52bd108886d1a854ae7db16aba5e4a
adam wojciechowski <adi61044@gmail.com>
2019-12-26 22:05:20 +0100

    Tabu.initialize() cleanup method added

--- TSPTabu/Tabu.py
+++ TSPTabu/Tabu.py
@@ -9,18 +9,23 @@ class Tabu:
     #uValue: value that objective function value should decrease by during uIter iterations
     #aspiration: value which swaping a banned pair of vertices should decrease the objective function value by
     #to ignore the fact that the pair is currently in tabu matrix
-    def __init__(self, inputVector, maxIter, uIter, uValue, aspiration):
-        self.maxIter = maxIter
-        self.aspiration = aspiration
+    def __init__(self, inputVector, **kwargs):
+        self.maxIter = kwargs['maxIter']
+        self.uIter = kwargs['uIter']
+        self.uValue = kwargs['uValue']
+        self.aspiration = kwargs['aspiration']
         self.inputVector = inputVector
 
     def solveTabu(self):
-        self.initialInstance = Greedy(self.inputVector)
-        self.initialInstance.solveGreedy()
-        self.solution = self.initialInstance.solution
+        self.initialize()
         #                                           $$$$IMPORTANT$$$$
         #CONVENTION: from the moment of self.initialInstance being generated self.solution contains two instances
         #of the first vertex (as 0th and last element of list). this implies that the length of self.solution
         #is greater than actual solution vector with unique only vertices by exactly 1.
-        self.tabuMatrix = TabuMatrix(len(self.solution) - 1)
         self.powerset = PowerSet(self.solution)
+
+    def initialize(self):
+        self.initialInstance = Greedy(self.inputVector)
+        self.initialInstance.solveGreedy()
+        self.solution = self.initialInstance.solution
+        self.tabuMatrix = TabuMatrix(len(self.solution) - 1)
