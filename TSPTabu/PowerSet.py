a05ac7ea8fe680f35649b2aec94affe147bed552
adam wojciechowski <adi61044@gmail.com>
2020-01-05 20:57:23 +0100

    added ugly functionality for terminating PowerSet.genAllPairs() function when powerset is generated for longer than 180 seconds

--- TSPTabu/PowerSet.py
+++ TSPTabu/PowerSet.py
@@ -7,11 +7,14 @@ should be deleted after each iteration of TSPTabu main for loop.
 
 from copy import deepcopy
 from TSPTabu.SwapPair import SwapPair
+from time import time
 
 class PowerSet:
     def __init__(self, vec: list()):
         self.vec = deepcopy(vec)
         self.pairs = list()
+        self.firstClock = 0
+        self.secondClock = 0
         self.genAllPairs()
         self.pairs.sort(key = lambda x : x.objFunctValue)
 
@@ -19,6 +22,12 @@ class PowerSet:
     #since first and last element of self.vec is duplicate of each other
     #and by assumption should be fixed in place
     def genAllPairs(self):
+        self.firstClock = time()
         for i in range(1, len(self.vec) - 1):
             for j in range(i + 1, len(self.vec) - 1):
                 self.pairs.append(SwapPair(self.vec, i, j))
+                self.secondClock = time()
+                self.elapsed = self.secondClock - self.firstClock
+                #print(self.elapsed)
+            if self.elapsed > 180:
+                break
